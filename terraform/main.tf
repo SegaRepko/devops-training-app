4353587c070c287a43e318892eb6c2a365ca2e3c
Ubuntu <ubuntu@ip-172-31-24-147.eu-central-1.compute.internal>
2023-09-13 14:48:30 +0000

    Replacing a server for PostgreSQL

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,27 +2,20 @@ provider "aws" {
   region = "eu-central-1"  
 }
 
-resource "aws_db_instance" "database" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "13.11"
-  instance_class       = "db.t3.micro"
-  identifier           = "mydb"
-  username             = "myuser"
-  password             = "mypassword"
-  license_model       = "postgresql-license"
-
-  tags = {
-    Name = "MyDBInstance"
-  }
-}
-
-resource "aws_instance" "web" {
+resource "aws_instance" "postgres" {
   ami           = "ami-04e601abe3e1a910f"
   instance_type = "t2.micro"
   key_name      = "aws-new-key-first" 
   tags = {
-    Name = "WebInstance"
+    Name = "PostgresInstance"
+  }
+}
+
+resource "aws_instance" "web-app" {
+  ami           = "ami-04e601abe3e1a910f"
+  instance_type = "t2.micro"
+  key_name      = "aws-new-key-first" 
+  tags = {
+    Name = "Web-App"
   }
 }
